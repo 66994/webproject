750b3e9fcfcb382ff37f44a528af2b46e5eb1132
Shane <1554096735@qq.com>
2025-10-14 20:15:01 +0800

    init

---
+++ src/main/java/com/digital/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.digital.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.digital.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
