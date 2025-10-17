f60be82cd70fba85c38b780c8ca2f62fdb018f87
zhaoshuang <1554096735@qq.com>
2025-10-18 00:33:31 +0800

    更新数据库建表语句

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,28 +1,27 @@
 # 数据库初始化
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists digital_college_students_backend;
 
 -- 切换库
-use my_db;
+use digital_college_students_backend;
 
 -- 用户表
 create table if not exists user
 (
     id           bigint auto_increment comment 'id' primary key,
-    userAccount  varchar(256)                           not null comment '账号',
+    userAccount  varchar(256)                           not null comment '账号（邮箱或者手机号）',
     userPassword varchar(512)                           not null comment '密码',
-    unionId      varchar(256)                           null comment '微信开放平台id',
-    mpOpenId     varchar(256)                           null comment '公众号openId',
     userName     varchar(256)                           null comment '用户昵称',
     userAvatar   varchar(1024)                          null comment '用户头像',
     userProfile  varchar(512)                           null comment '用户简介',
+    userPhone    varchar(512)                           null comment '用户手机号',
+    userEmail    varchar(512)                           null comment '用户邮箱',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
-    index idx_unionId (unionId)
-) comment '用户' collate = utf8mb4_unicode_ci;
+    isDelete     tinyint      default 0                 not null comment '是否删除'
+) comment '用户表' collate = utf8mb4_unicode_ci;
 
 -- 帖子表
 create table if not exists post
